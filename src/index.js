2cc5c00ceed1c5d53b3e681316cd604eb01674db
SAK74 <sergijks@gmail.com>
2021-06-22 19:12:16 +0200

    ...

--- src/index.js
+++ src/index.js
@@ -2,19 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
-import Main from './Pixabay';
+
 // import {SendFetch} from './Pixabay';
+import Main from './Pixabay';
+import store from './STORES/pixabayStore';
+
 import { Provider } from 'react-redux';
-import store from '../src/STORES/pixabayStore';
-// import { Counter } from './features/Counter';
+// import store from './STORES/counterStore';
+
+// import { CounterTry } from './CounterTry';
+
+// import {Main} from './TryRedux';
+// import store from './TRY_ReduxApp/STORE/store';
+// import { fetchUsers } from './TRY_ReduxApp/features/USERS/usersSlice';
 
 //import Toggle from './toggle';
-//import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
+
+// store.dispatch(fetchUsers());
+// console.log("index: ", store.getState());
 
 ReactDOM.render(
   <Provider store = {store}>
     <Main/>
-    {/* <SendFetch/> */}
   </Provider>
     ,
   document.getElementById('myDiv')
@@ -25,4 +35,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
-//reportWebVitals();
+// reportWebVitals(console.log);
