023961a6cecaf79798d97a99a79d4eb5b21839c6
SAK74 <sergijks@gmail.com>
2021-07-04 00:27:00 +0200

    Delete src/TRY_ReduxApp directory

--- src/TRY_ReduxApp/STORE/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import postReducer from '../features/POSTS/postSlice';
-import userReducer from '../features/USERS/usersSlice';
-
-export default configureStore({reducer: {
-    posts: postReducer,
-    users: userReducer
-}});
--- src/TRY_ReduxApp/features/fetch.js
+++
@@ -1,7 +0,0 @@
-import { createAsyncThunk } from "@reduxjs/toolkit"
-
-export const fetchPosts = createAsyncThunk("fetchPosts", async () => {
-    const resp = await fetch('http://localhost:3000/posts');
-    // console.log('resp: ', resp);
-    return await resp.json();
-});
