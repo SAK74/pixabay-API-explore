129f4202a9a6d2ebf840a591c980b852d7ddc05a
SAK74 <sergijks@gmail.com>
2021-07-04 01:01:04 +0200

    Delete Theme-context.js

--- src/Theme-context.js
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-
-export const content = {
-    blue: {font: 'blue', border: 'yellow'},
-    red: {font: 'red', border: 'black'}
-}
-export const MyContent = createContext({styl: content.blue, change: () => {}});
-
