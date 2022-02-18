f8a0865126913cb495afab055de3af5293b94c90
SAK74 <sergijks@gmail.com>
2022-02-18 20:55:54 +0100

    redirect request with proxy

--- src/pixaComponent/imagesSlice.js
+++ src/pixaComponent/imagesSlice.js
@@ -1,48 +1,81 @@
-import { createAsyncThunk, createEntityAdapter, createSlice } from "@reduxjs/toolkit";
+import {
+  createAsyncThunk,
+  createEntityAdapter,
+  createSlice
+} from "@reduxjs/toolkit";
+import axios from "axios";
 
-export const sendFetch = createAsyncThunk('sendFetch', async (perPage, { getState }) => {
-    const KEY = process.env.REACT_APP_API_KEY;
-    const { search, imageType, orientation, category, page, colors } = getState().fetch.params;
+export const sendFetch = createAsyncThunk(
+  "sendFetch",
+  (perPage, { getState }) => {
+    // const KEY = process.env.REACT_APP_API_KEY;
+    const {
+      search,
+      imageType,
+      orientation,
+      category,
+      page,
+      colors
+    } = getState().fetch.params;
     const lang = getState().fetch.lang;
-    const resp = await fetch(`https://pixabay.com/api/?key=${KEY}&page=${page}${search ? '&q=' + search : ''}&per_page=${perPage}&image_type=${imageType}&orientation=${orientation}&category=${category}&colors=${colors}&lang=${lang}`);
-    // console.log(resp);
-    if (!resp.ok) throw new Error("Error... " + resp.status + resp.statusText);
-    return resp.json();
-});
+    return axios({
+      url: `https://enylbkqka6yz1qs.m.pipedream.net`,
+      params: {
+        page,
+        q: search,
+        per_page: perPage,
+        image_type: imageType,
+        orientation,
+        category,
+        colors,
+        lang
+      }
+    })
+      .then((resp) => resp.data)
+      .catch((err) => {
+        console.log(err);
+        throw Error(err.message);
+      });
+  }
+);
 
 const imagesAdapter = createEntityAdapter();
 const initialState = imagesAdapter.getInitialState({
-    total: 0,
-    status: "iddle",
-    error: null
+  total: 0,
+  status: "iddle",
+  error: null
 });
 
 const imagesSlice = createSlice({
-    name: "images",
-    initialState,
-    reducers: {
-        resetStatus: state => { state.status = "iddle" }
-    },
-    extraReducers: {
-        [sendFetch.pending]: state => { state.status = "loading" },
-        [sendFetch.fulfilled]: (state, action) => {
-            state.status = "iddle";
-            state.total = action.payload.totalHits;
-            imagesAdapter.setAll(state, action.payload.hits);
-            console.log("STATE: ", action.payload.hits, state.status);
-        },
-        [sendFetch.rejected]: (state, action) => {
-            state.status = "failed";
-            state.error = action.error.message;
-            console.log(state.error);
-        }
+  name: "images",
+  initialState,
+  reducers: {
+    resetStatus: (state) => {
+      state.status = "iddle";
     }
+  },
+  extraReducers: {
+    [sendFetch.pending]: (state) => {
+      state.status = "loading";
+    },
+    [sendFetch.fulfilled]: (state, action) => {
+      state.status = "iddle";
+      state.total = action.payload.totalHits;
+      imagesAdapter.setAll(state, action.payload.hits);
+      console.log("STATE: ", action.payload.hits, state.status);
+    },
+    [sendFetch.rejected]: (state, action) => {
+      state.status = "failed";
+      state.error = action.error.message;
+      console.log(state.error);
+    }
+  }
 });
 
 export const {
-    selectAll: selectAllImages,
-    selectById: selectImageById
-} = imagesAdapter.getSelectors(state => state.images);
+  selectAll: selectAllImages,
+  selectById: selectImageById
+} = imagesAdapter.getSelectors((state) => state.images);
 // export const {resetStatus} = imagesSlice.actions;
 
 export default imagesSlice.reducer;
